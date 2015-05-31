e74801c7a6d04062494281d611fdd425fe2df471
Thomas Pöchtrager <t.poechtrager@gmail.com>
2015-05-31 19:56:19 +0200

    p7zip 9.38.1

---
+++ Utils/file_P7ZIP.py
@@ -0,0 +1,17 @@
+
+files_c=[
+ 'C/Threads.c',
+]
+
+files_cpp=[
+ 'CPP/7zip/UI/P7ZIP/wxP7ZIP.cpp',
+ 'CPP/Common/IntToString.cpp',
+ 'CPP/Common/MyString.cpp',
+ 'CPP/Common/MyVector.cpp',
+ 'CPP/Common/StringConvert.cpp',
+ 'CPP/Windows/FileDir.cpp',
+ 'CPP/Windows/FileFind.cpp',
+ 'CPP/Windows/FileIO.cpp',
+ 'CPP/myWindows/wine_date_and_time.cpp',
+]
+
---
+++ Utils/file_TestUI.py
@@ -0,0 +1,15 @@
+
+files_c=[
+ 'C/Threads.c',
+]
+
+files_cpp=[
+ 'CPP/7zip/TEST/TestUI/TestUI.cpp',
+ 'CPP/7zip/UI/FileManager/PasswordDialog.cpp',
+ 'CPP/7zip/UI/FileManager/PasswordDialog_rc.cpp',
+ 'CPP/Common/MyString.cpp',
+ 'CPP/Windows/Control/Controls.cpp',
+ 'CPP/Windows/Control/Dialog.cpp',
+ 'CPP/Windows/Window.cpp',
+]
+
